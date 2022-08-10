3ae2b4ad081c13abfd33c90d13c562f9f26d9613
Jimmy Nguyễn <84088181+cityuongbi@users.noreply.github.com>
2022-08-11 00:37:57 +0700

    Add sounds

--- iOS_App_Xylophone/ViewController.swift
+++ iOS_App_Xylophone/ViewController.swift
@@ -3,6 +3,7 @@
 //  Created by Nguyễn Mạnh Quân on 10/08/2022.
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController
 {
@@ -11,10 +12,32 @@ class ViewController: UIViewController
     {
         super.viewDidLoad()
     }
+    
+    var player: AVAudioPlayer?
 
     @IBAction func keyCPressed(_ sender: UIButton)
     {
-        
+        playSound()
+    }
+    
+    func playSound()
+    {
+        guard let path = Bundle.main.path(forResource: "C", ofType:"wav")
+        else
+        {
+            return
+        }
+        let url = URL(fileURLWithPath: path)
+        do
+        {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        }
+        catch let error
+        {
+            print(error.localizedDescription)
+        }
     }
     
 }
